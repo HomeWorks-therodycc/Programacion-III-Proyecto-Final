7bf8080e18a308836745d7c6cf6f4f1cfa3fdbe5
therodycc <therodycc@gmail.com>
2021-04-14 09:32:01 -0400

    Agregando menu

--- wwwroot/js/main.js
+++ wwwroot/js/main.js
@@ -1,11 +1,16 @@
 'use strict';
 
 
-const abrir = document.getElementById("BtnVerUsuarios");
-const tablamodal =document.getElementById("tablamodal");
+const abrir = document.getElementById('BtnVerUsuarios');
+const tablamodal =document.getElementById('tablamodal');
 
 
 abrir.onclick = ()=>{
     alert("Estas dando click");
-    tablamodal.style.display = "block";
+    tablamodal.style.display = 'block';
 }
+
+abrir.onclick = function(){
+    alert("Estas dando click");
+    tablamodal.style.display = "block";
+};
