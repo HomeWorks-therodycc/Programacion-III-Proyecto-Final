a372c5f5c3cb2a496cff17c01d3af6b57a83fb85
Ezequiel PR <knucquiel@gmail.com>
2021-04-21 14:47:33 -0400

    avatar para usuarios, cambios al registro de usuarios, arreglo a la img de la alerta de borrado de clientes y vehiculos, y checkbox para editar en el listado y las paginas dedicadas

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -7,6 +7,9 @@ using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
@@ -73,6 +76,9 @@ namespace Proyecto_final.Areas.Identity.Pages.Account
                 FormatErrorMessage = "La Cedula NO debe tener letras",
                 ValidErrorMessage = "La Cedula ingresada no es valida")]
             public string Cedula { get; set; }
+
+            [Required]
+            public IFormFile Foto { get; set; }
         }
 
         public async Task OnGetAsync(string returnUrl = null)
@@ -87,38 +93,49 @@ namespace Proyecto_final.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
+                var format = "image/jpg";
+                var resizedImage = Input.Foto;
+                var buffer = new byte[resizedImage.Length];
+                await resizedImage.OpenReadStream().ReadAsync(buffer);
+                var imageUri = $"data:{format};base64,{Convert.ToBase64String(buffer)}";
+
                 var user = new AppUser() {
                     UserName = Input.Email,
                     Email = Input.Email,
                     Nombre = Input.Nombre,
                     Apellido = Input.Apellido,
                     Cedula = Input.Cedula,
+                    Foto = imageUri,
+                    EmailConfirmed = true
                 };
+
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
 
-                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                    code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-                    var callbackUrl = Url.Page(
-                        "/Account/ConfirmEmail",
-                        pageHandler: null,
-                        values: new { area = "Identity", userId = user.Id, code = code, returnUrl = returnUrl },
-                        protocol: Request.Scheme);
+                    return Redirect("~/listado-usuarios");
 
-                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
-
-                    if (_userManager.Options.SignIn.RequireConfirmedAccount)
-                    {
-                        return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
-                    }
-                    else
-                    {
-                        await _signInManager.SignInAsync(user, isPersistent: false);
-                        return LocalRedirect(returnUrl);
-                    }
+//                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+//                    code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+//                    var callbackUrl = Url.Page(
+//                        "/Account/ConfirmEmail",
+//                        pageHandler: null,
+//                        values: new { area = "Identity", userId = user.Id, code = code, returnUrl = returnUrl },
+//                        protocol: Request.Scheme);
+//
+//                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
+//                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking //here</a>.");
+//
+//                    if (_userManager.Options.SignIn.RequireConfirmedAccount)
+//                    {
+//                        return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
+//                    }
+//                    else
+//                    {
+//                        await _signInManager.SignInAsync(user, isPersistent: false);
+//                        return LocalRedirect(returnUrl);
+//                    }
                 }
                 foreach (var error in result.Errors)
                 {
---
+++ Data/Migrations/20210421163422_imagen-usuario.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Proyecto_final.Data.Migrations
+{
+    public partial class imagenusuario : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nombre",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Apellido",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Foto",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Foto",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nombre",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Apellido",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Type",
+                table: "AspNetUsers",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
